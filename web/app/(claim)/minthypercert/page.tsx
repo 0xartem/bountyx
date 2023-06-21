20c35f4ba9ebfcda2cbff06a970849be87e72591
AV <artem.bv@pm.me>
2023-06-21 16:58:40 +0300

    chore: fix build

--- web/app/(claim)/minthypercert/page.tsx
+++ web/app/(claim)/minthypercert/page.tsx
@@ -3,14 +3,9 @@
 // @ts-nocheck
 import { useState, useEffect } from 'react'
 import { BountyxMetadata } from '@/bountyxlib/types/bountyxdata'
-import { CreateProjectForm } from '@/components/bountyx/create-project-form'
 
 export default function MintHypercert() {
   const [bounties, setBounties] = useState<BountyxMetadata[]>([])
 
-  return (
-    <div className="flex flex-col">
-      <CreateProjectForm />
-    </div>
-  )
+  return <div className="flex flex-col"></div>
 }
