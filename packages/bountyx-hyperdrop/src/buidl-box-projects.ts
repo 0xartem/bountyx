34337a73fe09ba927f5f1e38bef38637531e5073
AV <artem.bv@pm.me>
2023-09-21 18:02:35 +0200

    feat: pull data from hackathons

---
+++ packages/bountyx-hyperdrop/src/buidl-box-projects.ts
@@ -0,0 +1,64 @@
+import axios from "axios";
+import { writeFileSync } from "fs";
+
+const hackathons = {
+  ["7188260a-2241-4b6f-b8ab-f71842dbe944"]: "Eth Denver 2023"
+}
+
+const getHackathons = async (size: number): Promise<any[]> => {
+  const response = await axios.get(
+    `https://api.buidlbox.io/buidlbox/v1/hackathons?size=${size}`
+  )
+  return response.data.hackathons
+}
+
+const findHackathonByName = (hackathons: any[], hackathonName: string): any|undefined => {
+  return hackathons.find(hackathon => hackathon.hackathonName === hackathonName)
+}
+
+const getChallengesByPage = async (hackathonId: string, page: number): Promise<any[]> => {
+  const response = await axios.get(
+    `https://api.buidlbox.io/buidlbox/v1/hackathons/${hackathonId}/challenges?page=${page}`
+  );
+  return response.data.challenges;
+};
+
+const getWinnersByChallenge = async (hackathonId: string, challengeId: string): Promise<any> => {
+  const response = await axios.get(
+    `https://api.buidlbox.io/buidlbox/v1/hackathons/${hackathonId}/challenges/${challengeId}/projects?isWinner=true}`
+  );
+  return response.data.projects;
+};
+
+const getAllChallenges = async (hackathonId: string): Promise<any> => {
+  const allChallenges: any = [];
+  let challenges: any[] = await getChallengesByPage(hackathonId, 1);
+  for (let i = 2; challenges.length > 0; i++) {
+    for (let challenge of challenges) {
+      const dupChallenge = allChallenges.find((newChallenge: any) => newChallenge.challengeId === challenge.challengeId)
+      if (dupChallenge) {
+        console.log('Dup', dupChallenge.name)
+        continue
+      }
+      const winners = await getWinnersByChallenge(hackathonId, challenge.challengeId);
+      challenge.winners = winners;
+      allChallenges.push(challenge);
+    }
+    console.log('Length: ', allChallenges.length)
+    challenges = await getChallengesByPage(hackathonId, i);
+  }
+  return allChallenges;
+};
+
+const getEthDenverChallengesAndWinners = async (limit: number): Promise<any> => {
+  const hackathons = await getHackathons(limit)
+  const targetHackathon = findHackathonByName(hackathons, "ETHDenver 2023")
+  return await getAllChallenges(targetHackathon.hackathonId)
+}
+
+getEthDenverChallengesAndWinners(100)
+  .then((challenges) => {
+    console.log(challenges.length);
+    writeFileSync("challenges-and-winners.json", JSON.stringify(challenges));
+  })
+  .catch((err) => {});
