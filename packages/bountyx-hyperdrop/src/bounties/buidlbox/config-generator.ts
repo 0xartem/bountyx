8af72cc5cb832efa2f63258ea75cc2507d731674
AV <artem.bv@pm.me>
2023-09-21 18:04:00 +0200

    feat: pull bounties

--- packages/bountyx-hyperdrop/src/bounties/buidlbox/config-generator.ts
+++ packages/bountyx-hyperdrop/src/bounties/buidlbox/config-generator.ts
@@ -1,51 +1,49 @@
 import { BountyxMetadata } from "../../types/bountyxdata";
 import { BountyxMerkleLeafData } from "../../types/bountyxmerkleleafdata";
-import { groupedBountyData } from "./buidlbox-bounties";
+import challengesAndWinners from "../../../challenges-and-winners.json"
 import getWinnerAddress from "./winner-address-api";
 
-const convertToBountyxMetadata = (): BountyxMetadata[] => {
+const convertToBountyxMetadata = (groupName: string): BountyxMetadata[] => {
   const bountiesMetadata: BountyxMetadata[] = [];
 
-  for (const groupedBounties of groupedBountyData) {
-    for (const bounty of groupedBounties.bounties) {
-      const partialBountyXMetadata = {
-        group: groupedBounties.group.groupDisplayName,
-        name: bounty.name,
-        description: bounty.description,
-        issuer: {
-          issuerAddress: "0x0000000000000000000000000000000000000000",
-          issuerName: bounty.submittedByOrgName,
-          issuerLogoUrl: bounty.submittedByOrgLogo
+  for (const bounty of challengesAndWinners) {
+    const partialBountyXMetadata = {
+      group: groupName,
+      name: bounty.name,
+      description: bounty.description,
+      issuer: {
+        issuerAddress: "0x0000000000000000000000000000000000000000",
+        issuerName: bounty.submittedByOrgName,
+        issuerLogoUrl: bounty.submittedByOrgLogo
+      }
+    };
+
+    if (bounty.rewardPool) {
+      bountiesMetadata.push({
+        ...partialBountyXMetadata,
+        // receiver: {
+        //   receiverAddress: "0x0000000000000000000000000000000000000000"
+        // },
+        reward: {
+          rewardAmount: bounty.rewardPool,
+          rewardToken: bounty.rewardToken,
+          rewardInToken: false
         }
-      };
+      });
+    }
 
-      if (bounty.rewardPool) {
-        bountiesMetadata.push({
-          ...partialBountyXMetadata,
-          // receiver: {
-          //   receiverAddress: "0x0000000000000000000000000000000000000000"
-          // },
-          reward: {
-            rewardAmount: parseInt(bounty.rewardPool),
-            rewardToken: bounty.rewardToken,
-            rewardInToken: false
-          }
-        });
-      }
-
-      for (const reward of bounty.rewards) {
-        bountiesMetadata.push({
-          ...partialBountyXMetadata,
-          // receiver: {
-          //   receiverAddress: "0x0000000000000000000000000000000000000000"
-          // },
-          reward: {
-            rewardAmount: parseInt(reward.rewardAmountUsd),
-            rewardToken: bounty.rewardToken,
-            rewardInToken: false
-          }
-        });
-      }
+    for (const reward of bounty.rewards) {
+      bountiesMetadata.push({
+        ...partialBountyXMetadata,
+        // receiver: {
+        //   receiverAddress: "0x0000000000000000000000000000000000000000"
+        // },
+        reward: {
+          rewardAmount: reward.rewardAmountUsd,
+          rewardToken: bounty.rewardToken,
+          rewardInToken: false
+        }
+      });
     }
   }
 
@@ -53,7 +51,7 @@ const convertToBountyxMetadata = (): BountyxMetadata[] => {
 };
 
 export const getHyperdropLeavesData = (): BountyxMerkleLeafData[] => {
-  const bounties: BountyxMetadata[] = convertToBountyxMetadata();
+  const bounties: BountyxMetadata[] = convertToBountyxMetadata("ETHDenver 2023");
   const bountyxMerkleLeafs: BountyxMerkleLeafData[] = [];
   for (const bounty of bounties) {
     bountyxMerkleLeafs.push({
